950bb981d5d1abf65fd1ad9d069ca2942009ea96
Andreas <anbc@itu.dk>
2023-10-25 17:02:50 +0200

    First implementation of Chitty Chat

--- Server/server.go
+++ Server/server.go
@@ -7,7 +7,6 @@ import (
 	"io"
 	"log"
 	"net"
-	"os"
 	"sync"
 
 	gRPC "github.com/seve0039/gRPC.git/proto"
@@ -16,129 +15,93 @@ import (
 )
 
 type Server struct {
-	gRPC.UnimplementedTemplateServer        // You need this line if you have a server
-	name                             string // Not required but useful if you want to name your server
-	port                             string // Not required but useful if your server needs to know what port it's listening to
-
-	incrementValue int64      // value that clients can increment.
-	mutex          sync.Mutex // used to lock the server to avoid race conditions.
+	gRPC.UnimplementedChittyChatServer
+	participants     map[string]gRPC.ChittyChat_BroadcastServer
+	participantMutex sync.RWMutex
+	name             string
+	port             string
+	lamportClock     int64
 }
 
-// flags are used to get arguments from the terminal. Flags take a value, a default value and a description of the flag.
-// to use a flag then just add it as an argument when running the program.
-var serverName = flag.String("name", "default", "Senders name") // set with "-name <name>" in terminal
-var port = flag.String("port", "5400", "Server port")           // set with "-port <port>" in terminal
+var serverName = flag.String("name", "default", "Server's name")
+var port = flag.String("port", "5400", "Server port")
 
 func main() {
-
-	// f := setLog() //uncomment this line to log to a log.txt file instead of the console
-	// defer f.Close()
-
-	// This parses the flags and sets the correct/given corresponding values.
 	flag.Parse()
 	fmt.Println(".:server is starting:.")
-
-	// launch the server
 	launchServer()
-
-	// code here is unreachable because launchServer occupies the current thread.
 }
 
 func launchServer() {
-	log.Printf("Server %s: Attempts to create listener on port %s\n", *serverName, *port)
-
-	// Create listener tcp on given port or default port 5400
 	list, err := net.Listen("tcp", fmt.Sprintf("localhost:%s", *port))
 	if err != nil {
-		log.Printf("Server %s: Failed to listen on port %s: %v", *serverName, *port, err) //If it fails to listen on the port, run launchServer method again with the next value/port in ports array
-		return
+		log.Fatalf("Failed to listen on port %s: %v", *port, err)
 	}
 
-	// makes gRPC server using the options
-	// you can add options here if you want or remove the options part entirely
-	var opts []grpc.ServerOption
-	grpcServer := grpc.NewServer(opts...)
-
-	// makes a new server instance using the name and port from the flags.
+	grpcServer := grpc.NewServer()
 	server := &Server{
-		name:           *serverName,
-		port:           *port,
-		incrementValue: 0, // gives default value, but not sure if it is necessary
+		name:         *serverName,
+		port:         *port,
+		participants: make(map[string]gRPC.ChittyChat_BroadcastServer),
 	}
 
-	gRPC.RegisterTemplateServer(grpcServer, server) //Registers the server to the gRPC server.
-
+	gRPC.RegisterChittyChatServer(grpcServer, server)
 	log.Printf("Server %s: Listening at %v\n", *serverName, list.Addr())
 
 	if err := grpcServer.Serve(list); err != nil {
 		log.Fatalf("failed to serve %v", err)
 	}
-	// code here is unreachable because grpcServer.Serve occupies the current thread.
 }
 
-// The method format can be found in the pb.go file. If the format is wrong, the server type will give an error.
-func (s *Server) Increment(ctx context.Context, Amount *gRPC.Amount) (*gRPC.Ack, error) {
-	// locks the server ensuring no one else can increment the value at the same time.
-	// and unlocks the server when the method is done.
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
-
-	// increments the value by the amount given in the request,
-	// and returns the new value.
-	s.incrementValue += int64(Amount.GetValue())
-	return &gRPC.Ack{NewValue: s.incrementValue}, nil
+func (s *Server) Join(ctx context.Context, joinReq *gRPC.JoinRequest) (*gRPC.JoinAck, error) {
+	ack := &gRPC.JoinAck{Message: fmt.Sprintf("Welcome to Chitty-Chat, %s!", joinReq.Name)}
+	s.broadcastMessage(fmt.Sprintf("Participant %s joined Chitty-Chat at Lamport time %d", joinReq.Name, s.incrementLamport()))
+	return ack, nil
 }
 
-func (s *Server) SayHi(msgStream gRPC.Template_SayHiServer) error {
+func (s *Server) Leave(ctx context.Context, leaveReq *gRPC.LeaveRequest) (*gRPC.LeaveAck, error) {
+	s.participantMutex.Lock()
+	defer s.participantMutex.Unlock()
+
+	delete(s.participants, leaveReq.Name)
+	s.broadcastMessage(fmt.Sprintf("Participant %s left Chitty-Chat at Lamport time %d", leaveReq.Name, s.incrementLamport()))
+
+	return &gRPC.LeaveAck{Message: "Goodbye!"}, nil
+}
+
+func (s *Server) Broadcast(stream gRPC.ChittyChat_BroadcastServer) error {
+	msg, err := stream.Recv()
+	if err != nil {
+		return err
+	}
+
+	s.participantMutex.Lock()
+	s.participants[msg.Name] = stream
+	s.participantMutex.Unlock()
+
 	for {
-		// get the next message from the stream
-		msg, err := msgStream.Recv()
-
-		// the stream is closed so we can exit the loop
+		msg, err := stream.Recv()
 		if err == io.EOF {
-			break
+			return nil
 		}
-		// some other error
 		if err != nil {
 			return err
 		}
-		// log the message
-		log.Printf("Received message from %s: %s", msg.ClientName, msg.Message)
+
+		s.broadcastMessage(fmt.Sprintf("%s: %s at Lamport time %d", msg.Name, msg.Message, s.incrementLamport()))
 	}
-
-	// be a nice server and say goodbye to the client :)
-	ack := &gRPC.Farewell{Message: "Goodbye"}
-	msgStream.SendAndClose(ack)
-
-	return nil
 }
 
-// Get preferred outbound ip of this machine
-// Usefull if you have to know which ip you should dial, in a client running on an other computer
-func GetOutboundIP() net.IP {
-	conn, err := net.Dial("udp", "8.8.8.8:80")
-	if err != nil {
-		log.Fatal(err)
+func (s *Server) broadcastMessage(message string) {
+	s.participantMutex.RLock()
+	defer s.participantMutex.RUnlock()
+
+	for _, participant := range s.participants {
+		participant.Send(&gRPC.ChatMessage{Message: message, Timestamp: s.lamportClock})
 	}
-	defer conn.Close()
-
-	localAddr := conn.LocalAddr().(*net.UDPAddr)
-
-	return localAddr.IP
 }
 
-// sets the logger to use a log.txt file instead of the console
-func setLog() *os.File {
-	// Clears the log.txt file when a new server is started
-	if err := os.Truncate("log.txt", 0); err != nil {
-		log.Printf("Failed to truncate: %v", err)
-	}
-
-	// This connects to the log file/changes the output of the log informaiton to the log.txt file.
-	f, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("error opening file: %v", err)
-	}
-	log.SetOutput(f)
-	return f
+func (s *Server) incrementLamport() int64 {
+	s.lamportClock++
+	return s.lamportClock
 }
--- client/client.go
+++ client/client.go
@@ -5,159 +5,105 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
-	"strconv"
 	"strings"
 
 	gRPC "github.com/seve0039/gRPC.git/proto"
-
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-// Same principle as in client. Flags allows for user specific arguments/values
-var clientsName = flag.String("name", "default", "Senders name")
+var clientsName = flag.String("name", "default", "Sender's name")
 var serverPort = flag.String("server", "5400", "Tcp server")
 
-var server gRPC.TemplateClient  //the server
-var ServerConn *grpc.ClientConn //the server connection
+var server gRPC.ChittyChatClient
+var ServerConn *grpc.ClientConn
 
 func main() {
-	//parse flag/arguments
 	flag.Parse()
-
 	fmt.Println("--- CLIENT APP ---")
 
-	//log to file instead of console
-	//f := setLog()
-	//defer f.Close()
-
-	//connect to server and close the connection when program closes
-	fmt.Println("--- join Server ---")
 	ConnectToServer()
 	defer ServerConn.Close()
 
-	//start the biding
+	joinServer()
+
+	go listenForBroadcasts()
+
 	parseInput()
 }
 
-// connect to server
 func ConnectToServer() {
-
-	//dial options
-	//the server is not using TLS, so we use insecure credentials
-	//(should be fine for local testing but not in the real world)
 	opts := []grpc.DialOption{
 		grpc.WithBlock(),
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 	}
 
-	//dial the server, with the flag "server", to get a connection to it
-	log.Printf("client %s: Attempts to dial on port %s\n", *clientsName, *serverPort)
 	conn, err := grpc.Dial(fmt.Sprintf(":%s", *serverPort), opts...)
 	if err != nil {
-		log.Printf("Fail to Dial : %v", err)
-		return
+		log.Fatalf("Fail to Dial : %v", err)
 	}
 
-	// makes a client from the server connection and saves the connection
-	// and prints rather or not the connection was is READY
-	server = gRPC.NewTemplateClient(conn)
+	server = gRPC.NewChittyChatClient(conn)
 	ServerConn = conn
-	log.Println("the connection is: ", conn.GetState().String())
+}
+
+func joinServer() {
+	_, err := server.Join(context.Background(), &gRPC.JoinRequest{Name: *clientsName})
+	if err != nil {
+		log.Fatalf("Failed to join server: %v", err)
+	}
 }
 
 func parseInput() {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Type the amount you wish to increment with here. Type 0 to get the current value")
-	fmt.Println("--------------------")
-
-	//Infinite loop to listen for clients input.
 	for {
 		fmt.Print("-> ")
-
-		//Read input into var input and any errors into err
 		input, err := reader.ReadString('\n')
 		if err != nil {
 			log.Fatal(err)
 		}
-		input = strings.TrimSpace(input) //Trim input
+		input = strings.TrimSpace(input)
 
-		if !conReady(server) {
-			log.Printf("Client %s: something was wrong with the connection to the server :(", *clientsName)
-			continue
+		sendChatMessage(input)
+	}
+}
+
+func sendChatMessage(message string) {
+	if message == "exit" {
+		server.Leave(context.Background(), &gRPC.LeaveRequest{Name: *clientsName})
+		os.Exit(0)
+	}
+
+	stream, err := server.Broadcast(context.Background())
+	if err != nil {
+		log.Println("Failed to send message:", err)
+		return
+	}
+
+	msg := &gRPC.ChatMessage{Name: *clientsName, Message: message}
+	stream.Send(msg)
+}
+
+func listenForBroadcasts() {
+	stream, err := server.Broadcast(context.Background())
+	if err != nil {
+		log.Println("Failed to get broadcast stream:", err)
+		return
+	}
+
+	for {
+		msg, err := stream.Recv()
+		if err == io.EOF {
+			return
 		}
-
-		//Convert string to int64, return error if the int is larger than 32bit or not a number
-		val, err := strconv.ParseInt(input, 10, 64)
 		if err != nil {
-			if input == "hi" {
-				sayHi()
-			}
-			continue
+			log.Println("Failed to receive broadcast:", err)
+			return
 		}
-		incrementVal(val)
+
+		fmt.Printf("[Lamport Timestamp: %d] %s\n", msg.Timestamp, msg.Message)
 	}
-}
-
-func incrementVal(val int64) {
-	//create amount type
-	amount := &gRPC.Amount{
-		ClientName: *clientsName,
-		Value:      val, //cast from int to int32
-	}
-
-	//Make gRPC call to server with amount, and recieve acknowlegdement back.
-	ack, err := server.Increment(context.Background(), amount)
-	if err != nil {
-		log.Printf("Client %s: no response from the server, attempting to reconnect", *clientsName)
-		log.Println(err)
-	}
-
-	// check if the server has handled the request correctly
-	if ack.NewValue >= val {
-		fmt.Printf("Success, the new value is now %d\n", ack.NewValue)
-	} else {
-		// something could be added here to handle the error
-		// but hopefully this will never be reached
-		fmt.Println("Oh no something went wrong :(")
-	}
-}
-
-func sayHi() {
-	// get a stream to the server
-	stream, err := server.SayHi(context.Background())
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	// send some messages to the server
-	stream.Send(&gRPC.Greeding{ClientName: *clientsName, Message: "Hi"})
-	stream.Send(&gRPC.Greeding{ClientName: *clientsName, Message: "How are you?"})
-	stream.Send(&gRPC.Greeding{ClientName: *clientsName, Message: "I'm fine, thanks."})
-
-	// close the stream
-	farewell, err := stream.CloseAndRecv()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	log.Println("server says: ", farewell)
-}
-
-// Function which returns a true boolean if the connection to the server is ready, and false if it's not.
-func conReady(s gRPC.TemplateClient) bool {
-	return ServerConn.GetState().String() == "READY"
-}
-
-// sets the logger to use a log.txt file instead of the console
-func setLog() *os.File {
-	f, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("error opening file: %v", err)
-	}
-	log.SetOutput(f)
-	return f
 }
